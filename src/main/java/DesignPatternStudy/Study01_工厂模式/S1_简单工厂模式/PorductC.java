7b8a6fae32369bf23c1a57b862453921756f1f80
limao <limao@meituan.com>
2019-05-17 17:03:03 +0800

    no message

---
+++ src/main/java/DesignPatternStudy/Study01_工厂模式/S1_简单工厂模式/PorductC.java
@@ -0,0 +1,12 @@
+package DesignPatternStudy.Study01_工厂模式.S1_简单工厂模式;
+
+/**
+ * @author
+ * @version 1.0
+ * @created 2019/5/17 下午3:55
+ **/
+public class PorductC implements Porduct {
+    public void createCar() {
+        System.out.println("car c");
+    }
+}
