c885dc3344c543ac79a58d907c460ee24a1f73bd
limao <limao@meituan.com>
2019-05-17 17:47:44 +0800

    no message

---
+++ src/main/java/DesignPatternStudy/Study02_模板方法模式/Football.java
@@ -0,0 +1,24 @@
+package DesignPatternStudy.Study02_模板方法模式;
+
+/**
+ * @author
+ * @version 1.0
+ * @created 2019/5/17 下午5:46
+ **/
+public class Football extends Game {
+
+    @Override
+    void endPlay() {
+        System.out.println("Football Game Finished!");
+    }
+
+    @Override
+    void initialize() {
+        System.out.println("Football Game Initialized! Start playing.");
+    }
+
+    @Override
+    void startPlay() {
+        System.out.println("Football Game Started. Enjoy the game!");
+    }
+}
